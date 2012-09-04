a9a962260a664bc9335338203ba07e0ffdd299a3
Simón Castillo <scastb@gmail.com>
2012-09-04 05:57:47 -0430

    adding a subscription list view

--- showmethemoney/views.py
+++ showmethemoney/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from subscription.models import Subscription
+
+class SubscriptionListView(ListView):
+    model = Subscription
+    context_object_name = 'subscription_list'
+    template_name = 'showmethemoney/list.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(SubscriptionListView, self).dispatch(*args, **kwargs)
