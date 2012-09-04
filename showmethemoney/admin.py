16c37cb75beff26bdb3ebe8500db5821042904df
Simón Castillo <scastb@gmail.com>
2012-09-04 05:47:31 -0430

    adding admin.py from upstream

---
+++ showmethemoney/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import Transaction
+class TransactionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'timestamp'
+    list_display = ('timestamp', 'id', 'event', _subscription, _user,, 'amount', 'comment')
+    list_display_links = ('timestamp', 'id')
+    list_filter = ('subscription', 'user')
+admin.site.register(Transaction, TransactionAdmin)
